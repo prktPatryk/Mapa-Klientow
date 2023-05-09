5a94594a8da3d7ac5c9a5162208fc84b6584faa8
prktPatryk <patryk.kowalik@easyripok.pl>
2023-05-09 14:26:36 +0200

    Mapa i Klienci PSZOK

--- map.js
+++ map.js
@@ -1,71 +1,185 @@
-var map = L.map('map').setView([52.14, 19.06], 7);
+var map = L.map('map').setView([52.06, 19.14], 7);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-
 // Związek Międzygminny „Gospodarka Komunalna” - Chrzanów
+var marker = L.marker([50.1411926, 19.4028502]).addTo(map);
+
 // Przedsiębiorstwo Gospodarki Komunalnej Sp. Z o.o. W Wierzbinku
+var marker = L.marker([52.4456388, 18.5076904]).addTo(map);
+
 // Urząd Gminy Chojnów
-// Usługi Komunalne Sp. z o.o. – UK w Środzie Wielkopolskiejg
+var marker = L.marker([52.0032350, 21.0703166]).addTo(map);
+
+// Usługi Komunalne Sp. z o.o. – UK w Środzie Wielkopolskiej
+var marker = L.marker([52.2284266, 17.277827]).addTo(map);
+
 // Gmina Czeremcha
+var marker = L.marker([52.5141029, 23.3522319]).addTo(map);
+
 // Zakład Usługowo - Handlowy, Kamil Duda (Gmina Rudnik)
+var marker = L.marker([50.0796271, 18.0021082]).addTo(map);
+
 // Gmina Rzepiennik Strzyżewski
+var marker = L.marker([49.8072487, 21.0351977]).addTo(map);
+
 // Gmina Tymbark
+var marker = L.marker([49.7293607, 20.3248972]).addTo(map);
+
 // Miasto Łaskarzew
+var marker = L.marker([51.7889644, 21.5921883]).addTo(map);
+
 // Związek Międzygminny „EKO SIÓDEMKA”
+var marker = L.marker([51.695926, 17.4374158]).addTo(map);
+
 // Urzędu Miasta Radzyń Podlaski
+var marker = L.marker([51.7827253, 22.6208818]).addTo(map);
+
 // Miasto Racibórz
+var marker = L.marker([50.0917343, 18.2196766]).addTo(map);
+
 // Miejskie Przedsiębiorstwo Wodociągów i Kanalizacji Sp. z o.o. w Sokółce
+var marker = L.marker([53.4049183, 23.4995562]).addTo(map);
+
 // Zakład Gospodarki Komunalnej w Ostrowi Mazowieckiej Sp. z.o.o.
+var marker = L.marker([52.8000429, 21.897638]).addTo(map);
+
 // Zakład usług komunalnych w Barczewie 
+var marker = L.marker([53.8297112, 20.6911497]).addTo(map);
+
 // Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o.o. Kazimierz Biskupi
+var marker = L.marker([52.3119341, 18.1675656]).addTo(map);
+
 // Gmina Nałęczów
+var marker = L.marker([51.2876064, 22.214792]).addTo(map);
+
 // Przedsiębiorstwo Produkcyjno-Handlowo-Usługowe "KOMART" sp. z o.o
+var marker = L.marker([50.2216536, 18.6706574]).addTo(map);
+
 // Lubelskie Przedsiębiorstwo Gospodarki Komunalnej
+var marker = L.marker([51.250559, 22.57010224]).addTo(map);
+
 // Zakład Wielobranżowy '' GIERAS '' Krzysztof Gieras
+var marker = L.marker([51.1142355, 18.2143288]).addTo(map);
+
 // PGKiM Sp. z o. o.  Nowa Dęba
+var marker = L.marker([50.413485, 21.7541268]).addTo(map);
+
 // Gmina Gorzów Śląski
+var marker = L.marker([51.0281551, 18.4222682]).addTo(map);
+
 // Gmina Bieżuń
+var marker = L.marker([52.9617996, 19.8899936]).addTo(map);
+
 // EKOLAND
+var marker = L.marker([50.9249236, 22.229206]).addTo(map);
+
 // Miasto i Gmina Sieniawa
+var marker = L.marker([50.1780728, 22.6102155]).addTo(map);
+
 // Wodociągi Rewal Sp z o.o.
+var marker = L.marker([54.0570808, 14.9332546]).addTo(map);
+
 // Przedsiębiorstwo Usług Komunalnych Sp. z o.o. Borne Sulinowo
+var marker = L.marker([53.5812939, 16.538117]).addTo(map);
+
 // Gmina Nisko
+var marker = L.marker([50.5198985, 22.139886]).addTo(map);
+
 // Gmina Bledzew
+var marker = L.marker([52.5175796, 15.413957]).addTo(map);
+
 // Brzeskie Zakłady Komunalne Sp. z o.o.
+var marker = L.marker([49.9678396, 20.6068496]).addTo(map);
+
 // Gmina Branice
+var marker = L.marker([50.0506887, 17.7940229]).addTo(map);
+
 // Przedsiębiorstwo Usług Komunalnych Sp. z o.o. Rumia
+var marker = L.marker([54.5700148, 18.3969008]).addTo(map);
+
 // Gmina Grójec
+var marker = L.marker([51.8655126, 20.8675017]).addTo(map);
+
 // Związek Gmin Wierzyca
+var marker = L.marker([53.9671805, 18.532113]).addTo(map);
+
 // Gmina Dzikowiec
+var marker = L.marker([50.2683524, 21.8543623]).addTo(map);
+
 // Gmina Kaliska
+var marker = L.marker([53.9051921, 18.2184048]).addTo(map);
+
 // Gmina Krzeszowice
-// Gmina Gorzów Śląski, Gmina Radłów
+var marker = L.marker([50.1350246, 19.6320164]).addTo(map);
+
+// Gmina Radłów
+var marker = L.marker([50.9330577, 18.5346776]).addTo(map);
+
 // Technika Komunalna w Górze
+var marker = L.marker([51.6681499, 16.5393277]).addTo(map);
+
 // Zakład Usług Komunalnych "EKO-GAB" s.c. G. Kropidłowski, D. Piąstka
+var marker = L.marker([51.9386721, 18.534998]).addTo(map);
+
 // Gmina Szczecinek
+var marker = L.marker([53.708021, 16.6943922]).addTo(map);
+
 // Gospodarka Komunalna w Błażowej Sp. z o.o.
+var marker = L.marker([49.8856022, 22.1008013]).addTo(map);
+
 // Gospodarka Komunalna Sp. z o. o. W Olsztynku/Pcnet Grześkiewicz Zaliński Spółka Jawna
+var marker = L.marker([53.7766839, 20.476507]).addTo(map);
+
 // Gmina Pawłowice
+var marker = L.marker([49.9608911, 18.7161904]).addTo(map);
+
 // Gmina Ustronie Morskie
+var marker = L.marker([54.2135984, 15.7560763]).addTo(map);
+
 // Gmina Kunów
+var marker = L.marker([50.9541063, 21.278823]).addTo(map);
+
 // Koźmińskie Usługi Komunalne Sp. z o.o.
+var marker = L.marker([51.8256567, 17.4528473]).addTo(map);
+
 // Zakład Usług Komunalnych Jasieniec
+var marker = L.marker([51.8224743, 20.9354868]).addTo(map);
+
 // Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o. o. Ozimek
+var marker = L.marker([50.692892, 18.1961801]).addTo(map);
+
 // Miasto Radzyń Podlaski
+var marker = L.marker([51.7827253, 22.6208818]).addTo(map);
+
 // Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o.o. Strzyżów
+var marker = L.marker([49.8702087, 21.7856291]).addTo(map);
+
 // Gmina Jadów
+var marker = L.marker([52.4783475, 21.6326228]).addTo(map);
+
 // Jedlickie Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o. o.
+var marker = L.marker([49.714148, 21.6421513]).addTo(map);
+
 // Gmina Nawojowa
+var marker = L.marker([49.55912, 20.74119]).addTo(map);
+
 // Gmina Pietrowice Wielkie
-// Przedsiębiorstwo Usług Komunalnych Kaliska Sp. z o.o. w organizacji
-// Gordon & Great Consulting Sp. z o.o. Sp. k.
-// Miasto i Gmina Pilawa
-// Gmina Laskowa
-// Gmina Chojna
+var marker = L.marker([50.0844961, 18.0906235]).addTo(map);
+
 // Przedsiębiorstwo Usług Komunalnych Kaliska Sp. z o.o.
-// Gmina Wierzchosławice
-// Gmina Nisko 
+var marker = L.marker([53.9051921, 18.2184048]).addTo(map);
+
+/* Gmina Wierzchosławice *oraz* Gordon & Great Consulting Sp. z o.o. Sp. k. */
+var marker = L.marker([50.0274989, 20.8593482]).addTo(map);
+
+// Miasto i Gmina Pilawa
+var marker = L.marker([51.9595452, 21.5271524]).addTo(map);
+
+// Gmina Laskowa
+var marker = L.marker([49.7604444, 20.4507473]).addTo(map);
+
+// Gmina Chojna
+var marker = L.marker([52.9629793, 14.4314113]).addTo(map);
