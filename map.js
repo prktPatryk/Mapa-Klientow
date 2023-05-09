af7d0922d1f0d1d9e2783968223d33a98f763831
prktPatryk <patryk.kowalik@easyripok.pl>
2023-05-09 11:37:40 +0200

    Mapa i Klienci

---
+++ map.js
@@ -0,0 +1,71 @@
+var map = L.map('map').setView([52.14, 19.06], 7);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+
+// Związek Międzygminny „Gospodarka Komunalna” - Chrzanów
+// Przedsiębiorstwo Gospodarki Komunalnej Sp. Z o.o. W Wierzbinku
+// Urząd Gminy Chojnów
+// Usługi Komunalne Sp. z o.o. – UK w Środzie Wielkopolskiejg
+// Gmina Czeremcha
+// Zakład Usługowo - Handlowy, Kamil Duda (Gmina Rudnik)
+// Gmina Rzepiennik Strzyżewski
+// Gmina Tymbark
+// Miasto Łaskarzew
+// Związek Międzygminny „EKO SIÓDEMKA”
+// Urzędu Miasta Radzyń Podlaski
+// Miasto Racibórz
+// Miejskie Przedsiębiorstwo Wodociągów i Kanalizacji Sp. z o.o. w Sokółce
+// Zakład Gospodarki Komunalnej w Ostrowi Mazowieckiej Sp. z.o.o.
+// Zakład usług komunalnych w Barczewie 
+// Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o.o. Kazimierz Biskupi
+// Gmina Nałęczów
+// Przedsiębiorstwo Produkcyjno-Handlowo-Usługowe "KOMART" sp. z o.o
+// Lubelskie Przedsiębiorstwo Gospodarki Komunalnej
+// Zakład Wielobranżowy '' GIERAS '' Krzysztof Gieras
+// PGKiM Sp. z o. o.  Nowa Dęba
+// Gmina Gorzów Śląski
+// Gmina Bieżuń
+// EKOLAND
+// Miasto i Gmina Sieniawa
+// Wodociągi Rewal Sp z o.o.
+// Przedsiębiorstwo Usług Komunalnych Sp. z o.o. Borne Sulinowo
+// Gmina Nisko
+// Gmina Bledzew
+// Brzeskie Zakłady Komunalne Sp. z o.o.
+// Gmina Branice
+// Przedsiębiorstwo Usług Komunalnych Sp. z o.o. Rumia
+// Gmina Grójec
+// Związek Gmin Wierzyca
+// Gmina Dzikowiec
+// Gmina Kaliska
+// Gmina Krzeszowice
+// Gmina Gorzów Śląski, Gmina Radłów
+// Technika Komunalna w Górze
+// Zakład Usług Komunalnych "EKO-GAB" s.c. G. Kropidłowski, D. Piąstka
+// Gmina Szczecinek
+// Gospodarka Komunalna w Błażowej Sp. z o.o.
+// Gospodarka Komunalna Sp. z o. o. W Olsztynku/Pcnet Grześkiewicz Zaliński Spółka Jawna
+// Gmina Pawłowice
+// Gmina Ustronie Morskie
+// Gmina Kunów
+// Koźmińskie Usługi Komunalne Sp. z o.o.
+// Zakład Usług Komunalnych Jasieniec
+// Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o. o. Ozimek
+// Miasto Radzyń Podlaski
+// Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o.o. Strzyżów
+// Gmina Jadów
+// Jedlickie Przedsiębiorstwo Gospodarki Komunalnej i Mieszkaniowej Sp. z o. o.
+// Gmina Nawojowa
+// Gmina Pietrowice Wielkie
+// Przedsiębiorstwo Usług Komunalnych Kaliska Sp. z o.o. w organizacji
+// Gordon & Great Consulting Sp. z o.o. Sp. k.
+// Miasto i Gmina Pilawa
+// Gmina Laskowa
+// Gmina Chojna
+// Przedsiębiorstwo Usług Komunalnych Kaliska Sp. z o.o.
+// Gmina Wierzchosławice
+// Gmina Nisko 
